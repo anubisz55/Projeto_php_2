d82b61e87f3bb486b52db1259c6235f71191719a
anubisz55 <151870080+anubisz55@users.noreply.github.com>
2024-06-14 14:59:06 -0300

    atualizações

--- Projeto2-main/Sistema_PHP/listar-pacientes.php
+++ Projeto2-main/Sistema_PHP/listar-pacientes.php
@@ -87,7 +87,30 @@
                         echo "<li>Nome: " . $paciente_atual["nome"] . ", Telefone: " . $paciente_atual["telefone"] . "</li>";
                     }
                 }
-            ?>
+            
+            
+    $periodo = 2;
+ 
+        switch ($periodo) {
+             case 1:
+        echo 'Período identificado como manhã';
+        echo '<br>';
+        break;
+            case 2:
+        echo 'Período identificado como tarde';
+        echo '<br>';
+        break;
+            case 3:
+        echo 'Período identificado como noite';
+        echo '<br>';
+        break;
+            default:
+        echo 'Período não identificado';
+        echo '<br>';
+        break;
+}
+?>
+            
 
         </ul>
 
